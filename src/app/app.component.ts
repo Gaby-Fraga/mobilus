6b96c25ba4029ddc5212c5c6a5cb4d3c4c2ebc93
Gaby-Fraga <gabyfragamoreiramedeiros@gmail.com>
2021-02-03 20:12:22 -0300

    projeto mobilus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,82 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit{
   title = 'covid19';
-  cases = {};
+  cases = [];
+  avgCases = [];
+  monthCases = [];  
+  allCases = [];
+  allDeaths = [];
+  treeControl = new NestedTreeControl<any>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<any>();
 
   constructor(private appService: AppService){}
 
   ngOnInit(){
 
     this.appService.getCases().then((cases: any)=> {
-      debugger
       this.cases = cases;
+      this.calculateTwoWeekAvg();
+      this.calculateMonthCases();
+    });
+
+  }
+
+  calculateTwoWeekAvg(){
+
+    for(var x = 1; x <= 14; x++){
+      var weekCases = [];
+      for(var y = 0; y <= 7; y++){
+        weekCases.push(this.cases[this.cases.length - x - y]);
+      }
+      this.avgCases.push({weekCases: weekCases, day: this.cases[this.cases.length - x]});
+    }
+
+    this.avgCases.forEach(day => {
+      var weekDeathAvg = 0;
+      day.weekCases.forEach((element, index) => {
+        
+        if(index < 7){
+          var newDeaths = element.Deaths - day.weekCases[index + 1].Deaths;
+          weekDeathAvg += newDeaths;
+        }       
+      });
+      day.weekDeathAvg = weekDeathAvg / 7;
+    });
+
+    this.dataSource.data = this.avgCases;
+  }
+
+  calculateMonthCases(){
+
+    var mostCases = 0;
+    var mostDeaths = 0;
+
+    this.allCases = [];
+    this.allDeaths = [];
+
+    for(var x = 1; x < 31; x++){
+      var deaths = this.cases[this.cases.length - x].Deaths - this.cases[this.cases.length - x - 1].Deaths;
+      var confirmed = this.cases[this.cases.length - x].Confirmed - this.cases[this.cases.length - x - 1].Confirmed;
+      this.monthCases.push({day: this.cases[this.cases.length - x],
+      deaths: deaths, confirmed: confirmed});
+    }
+
+    this.monthCases.forEach(element => {
+      if(element.deaths > mostDeaths){
+        this.allDeaths = [];
+        this.allDeaths.push(element);
+        mostDeaths = element.deaths;
+      }else if (element.deaths == mostDeaths){
+        this.allDeaths.push(element);
+      }
+      
+      if(element.confirmed > mostCases){
+        this.allCases = [];
+        this.allCases.push(element);
+        mostCases = element.confirmed;
+      }else if (element.confirmed == mostCases){
+        this.allCases.push(element);
+      }
     });
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     MatTabsModule,
     BrowserAnimationsModule,
+    MatTreeModule,
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
